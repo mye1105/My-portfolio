c1d5710fe86033aab2f24f38d9d39798be02a96a
miao ye <miaoye@miaos-air.mynetworksettings.com>
2024-01-29 22:10:56 -0500

    homework of JS

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,16 @@
 let score=parseInt(prompt('Enter the score:'));
-if(score<=60){
-  console.log('Mark:A');
+if(score<60){
+  console.log('Mark:F');
 
 }
-else if(score)
+else if(score>=60 && score<70){
+  console.log('Mark:D');
+}
+else if(score>=70 && score<80){
+  console.log('Mark:C');
+}
+else if (score>=80 && score<90){
+  console.log('Mark:B');
+} else{
+  console.log('Mark:A');
+}
